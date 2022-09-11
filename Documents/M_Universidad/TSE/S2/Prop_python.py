4ae7485e3f75286f7c8efe184c6de6852eb594af
chabelarc <chabelarc@ciencias.unam.mx>
2022-09-11 03:34:00 -0500

    Primer ejercicio (2)

--- Documents/M_Universidad/TSE/S2/Prop_python.py
+++ Documents/M_Universidad/TSE/S2/Prop_python.py
@@ -7,3 +7,5 @@ x= datetime.datetime.now()
 
 print ("La fecha es:")
 print (x)
+print ("Año:",x.year,"Mes:",x.strftime("%B"),"Día:",x.strftime("%A"),x.strftime("%d"))
+print (x.strftime ("%H:%M:%S %p"))
